b4e2e73db0f0c02cf9bf956b6cf1a4726f8d737f
varun700 <varunmgeakminds@gmail.com>
2022-12-14 18:35:05 +0530

    first commit

---
+++ src/Layout/Sidebar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import { Layout, Menu, theme } from "antd";
+const { Header, Content, Footer, Sider } = Layout;
+const Sidebar = () => {
+  return (
+    <div className="sidebar-container">
+      <div className="sideba_logo">HI</div>
+    </div>
+  );
+};
+
+export default Sidebar;
